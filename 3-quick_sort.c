9759acc64f2be4ad18931abd11dfb32d8937075d
aureliewouy <aureliecedia@yahoo.com>
2021-02-01 13:43:01 +0000

    add documentation

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,11 @@
 #include "sort.h"
-
+/**
+ * swap - function that swap two number)
+ * @x: the first number
+ * @y: the secund number
+ *
+ * Return: Nothing
+ */
 void swap(int *x, int *y)
 {
 	int tmp = *x;
@@ -7,6 +13,15 @@ void swap(int *x, int *y)
 	*x = *y;
 	*y = tmp;
 }
+/**
+ * partition - partitions the array
+ * @a: the array
+ * @start: the start of the array the lower
+ * @end: the end of the array
+ * @size: size of the array
+ *
+ * Return: the pivot index
+ */
 
 int partition(int *a, int start, int end, size_t size)
 {
@@ -32,19 +47,27 @@ int partition(int *a, int start, int end, size_t size)
 		swap(&a[pindex], &a[end]);
 		print_array(a, size);
 	}
-	return pindex;
+	return (pindex);
 }
+/**
+ * quick_help - helps to index the be partition
+ * @array: the array
+ * @start: the lower
+ * @end: the higher
+ * @size: the size of the array
+ *
+ * Return: Nothing
+ */
 
-
-void quick(int *array, int start, int end, size_t size)
+void quick_help(int *array, int start, int end, size_t size)
 {
 	int pivot_index;
 
 	if (start >= end)
 		return;
 	pivot_index = partition(array, start, end, size);
-	quick(array, start, pivot_index - 1, size);
-	quick(array, pivot_index + 1, end, size);
+	quick_help(array, start, pivot_index - 1, size);
+	quick_help(array, pivot_index + 1, end, size);
 
 }
 
@@ -61,5 +84,5 @@ void quick(int *array, int start, int end, size_t size)
 void quick_sort(int *array, size_t size)
 {
 
-	quick(array, 0, size - 1, size);
+	quick_help(array, 0, size - 1, size);
 }
