a7f6441825c00acd207524739a4ff0b301491720
aureliewouy <aureliecedia@yahoo.com>
2021-02-01 13:04:21 +0000

    add verifications

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -11,21 +11,23 @@ void selection_sort(int *array, size_t size)
 {
 	size_t i, j, min_idx, tmp;
 
-	for (i = 0; i < size - 1; i++)
+	if ((array != NULL) && (size >= 2))
 	{
-		min_idx = i;
-		for (j = i + 1; j < size; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] < array[min_idx])
+			min_idx = i;
+			for (j = i + 1; j < size; j++)
 			{
-				min_idx = j;
-				print_array(array, size);
+				if (array[j] < array[min_idx])
+				{
+					min_idx = j;
+					print_array(array, size);
+				}
 			}
+			tmp = array[min_idx];
+			array[min_idx] = array[i];
+			array[i] = tmp;
 		}
-		tmp = array[min_idx];
-		array[min_idx] = array[i];
-		array[i] = tmp;
-
 	}
 
 }
