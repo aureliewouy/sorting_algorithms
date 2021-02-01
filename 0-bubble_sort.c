429b1a6a0dd9c205b7038e4b5df6210f58cf61d5
aureliewouy <aureliecedia@yahoo.com>
2021-02-01 13:00:51 +0000

    add verifications

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -27,14 +27,17 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, j;
 
-	for (i = 0; i < size - 1; i++)
+	if (array != NULL) && (size >= 2))
 	{
-		for (j = 0; j < size - 1; j++)
+		for (i = 0; i < size - 1; i++)
 		{
-			if (array[j] > array[j + 1])
+			for (j = 0; j < size - 1; j++)
 			{
-				swap(&array[j], &array[j + 1]);
-				print_array(array, size);
+				if (array[j] > array[j + 1])
+				{
+					swap(&array[j], &array[j + 1]);
+					print_array(array, size);
+				}
 			}
 		}
 	}
